7370c231b46512524cc1c0f0e4d92e9770f65244
CDayL <dayanaparck@gmail.com>
2023-03-09 13:46:12 -0600

    my first commit

---
+++ Holamundo.java
@@ -0,0 +1,15 @@
+/// Empezamos con el primer hola mundo
+
+// Siempre debe llamarse igual que el archivo
+public class Holamundo 
+{
+    ///Parte del menu principal, esta es estatica y se hace en cadena (String)
+    public static void main(String[] args) 
+    {
+        ///Sirve para imprimir algo
+        System.out.println("Hola mundo");
+        
+        ///Te saca de lo que estes realizando
+        return;
+    }
+}
